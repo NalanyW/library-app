8601859037fdef88167d8238dc14cefe4008cc8b
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-06-07 20:06:42 +0200

    made a single iteration test.

--- cypress/e2e/authentication.cy.js
+++ cypress/e2e/authentication.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-      cy.visit('https://localhost:7258/')
-  })
-})
+describe('LibraryMVC', () => {
+
+    //login attempt without correct email format
+    it('login', () => {
+        cy.visit('https://localhost:7258/');
+        cy.get('body > header > nav > div > div > ul:nth-child(2) > li:nth-child(2) > a').click();
+
+
+        cy.get('#Input_Email').type('test');
+        cy.get('#Input_Password').type('test');
+        
+        
+    });
+});
