cb2e39eb4b2288fe177a7b978046cc9b779627a5
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-06-12 13:55:59 +0200

    i made a seed for the teachers. but there needs to be a lot more seeds

--- Data/AppDbInitializer.cs
+++ Data/AppDbInitializer.cs
@@ -1,4 +1,6 @@
-﻿namespace SoftwareDevelopment2.Data
+﻿using SoftwareDevelopment2.Models;
+
+namespace SoftwareDevelopment2.Data
 {
 	public class AppDbInitializer
 	{
@@ -10,7 +12,89 @@
 
 				context.Database.EnsureCreated();
 
+				/*if (!context.Users.Any())
+				{
+					context.Users.AddRange(new List<User>()
+					{
+						new User()
+						{
+							name = "Test",
+							Age = "24"
+						}
+					});
+				}*/
 
+				/*if (!context.Student.Any())
+				{
+					context.Student.AddRange(new List<student>()
+				}*/
+
+				if (!context.Teacher.Any())
+				{
+					context.Teacher.AddRange(new List<Teacher>()
+					{
+						new Teacher()
+						{
+							FirstName = "Jan",
+							LastName = "Zuur",
+							Email = "placeholder"
+							/*there needs to be a connection to the course*/
+						},
+
+						new Teacher()
+						{
+							FirstName = "Jan",
+							LastName = "Zuur",
+							Email = "placeholder"
+						},
+
+						new Teacher()
+						{
+							FirstName = "Jan",
+							LastName = "Zuur",
+							Email = "placeholder"
+						},
+					});
+				};
+
+				if (!context.Roles.Any())
+				{
+
+				}
+
+				if (!context.Course.Any())
+				{
+
+				}
+
+				if (!context.Book.Any())
+				{
+
+				}
+
+				/*if (!context.UserRoles.Any())
+				{
+					context.UserRoles.AddRange(new List<UserRole>()
+					{
+						new UserRole()
+						{
+							name = "Test",
+							Age = "24"
+						}
+					});
+
+				}*/
+
+				if (!context.UserClaims.Any())
+				{
+
+				}
+
+				
+
+				
+
+				
 			}
 		}
 	}
