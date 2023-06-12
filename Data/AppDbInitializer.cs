647341c6589a54e27fc2c7c2689a551736ff74d2
giannivanlooij <54837793+giannivanlooij@users.noreply.github.com>
2023-06-12 18:05:16 +0200

    the seeder for the current tables in the database

--- Data/AppDbInitializer.cs
+++ Data/AppDbInitializer.cs
@@ -12,17 +12,6 @@ namespace SoftwareDevelopment2.Data
 
 				context.Database.EnsureCreated();
 
-				/*if (!context.Users.Any())
-				{
-					context.Users.AddRange(new List<User>()
-					{
-						new User()
-						{
-							name = "Test",
-							Age = "24"
-						}
-					});
-				}*/
 
 				if (!context.Student.Any())
 				{
@@ -84,44 +73,109 @@ namespace SoftwareDevelopment2.Data
 					});
 				};
 
-				if (!context.Roles.Any())
-				{
-
-				}
 
 				if (!context.Course.Any())
 				{
+					context.Course.AddRange(new List<Course>()
+					{
+						new Course()
+						{
+							Id = 1,
+							Title = "test",
+							Code = "test",
+							Description = "test"
+							/*there needs to be a connection to the student*/
+							/*there needs to be a connection to the teacher*/
+						},
 
+						new Course()
+						{
+							Id = 2,
+							Title = "test2",
+							Code = "test2",
+							Description = "test2"
+							/*there needs to be a connection to the student*/
+							/*there needs to be a connection to the teacher*/
+						},
+
+						new Course()
+						{
+							Id = 3,
+							Title = "test3",
+							Code = "test3",
+							Description = "test3"
+							/*there needs to be a connection to the student*/
+							/*there needs to be a connection to the teacher*/
+						},
+					});
 				}
 
 				if (!context.Book.Any())
 				{
+					context.Book.AddRange(new List<Book>()
+					{
+						new Book()
+						{
+							Title = "test1",
+							Author = "test1",
+							Price = 15,
+							YearOfRelease = 07121998,
+							Location = "verdieping1"
+						},
 
+						new Book()
+						{
+							Title = "test2",
+							Author = "test2",
+							Price = 15,
+							YearOfRelease = 07121998,
+							Location = "verdieping2"
+						},
+
+						new Book()
+						{
+							Title = "test3",
+							Author = "test3",
+							Price = 15,
+							YearOfRelease = 07121998,
+							Location = "verdieping3"
+						},
+					});
 				}
 
-				/*if (!context.UserRoles.Any())
+				/*if (!context.Item.Any())
 				{
-					context.UserRoles.AddRange(new List<UserRole>()
+					context.Item.AddRange(new List<Item>()
 					{
-						new UserRole()
+						new Item()
 						{
-							name = "Test",
-							Age = "24"
-						}
+							*//*there needs to be a connection to the course*//*
+						},
+
+						new Item()
+						{
+							*//*there needs to be a connection to the course*//*
+						},
+
+						new Item()
+						{
+							*//*there needs to be a connection to the course*//*
+						},
 					});
+				}*/
+
+				/*if (!context.Roles.Any())
+				{
 
 				}*/
 
-				if (!context.UserClaims.Any())
-				{
 
-				}
 
-				
 
-				
 
-				
+
+
+
 			}
 		}
 	}
